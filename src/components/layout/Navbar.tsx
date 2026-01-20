97d03ec64412db5319318f16cc76a837b16b90b5
Prince Kumar <2101772.cse.cec@cgc.edu.in>
2026-01-20 21:07:29 +0530

    updation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
           <img
               src={logo}
               alt="ITERAC Logo"
-              className="h-8 md:h-10 w-auto object-contain"
+              className="h-11 md:h-14 w-auto object-contain"
             />
           </Link>
 
